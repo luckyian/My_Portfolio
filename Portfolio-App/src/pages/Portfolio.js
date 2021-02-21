b0515364e896d270f727b35cc43211571d05d05c
Ian Wren <ian.wren81@gmail.com>
2021-02-21 11:34:51 -0800

    Cleaned folder structure to better represent file tree and planned final product

--- Portfolio-App/src/pages/Portfolio.js
+++ Portfolio-App/src/pages/Portfolio.js
@@ -37,23 +37,23 @@ class Portfolio extends Component {
   // };
   render() {
     return (
-      <div>
-        <Container style={{ minHeight: "80%" }}>
-          <h1 className="text-center">Search By Breed!</h1>
-          <Alert
-            type="danger"
-            style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
-          >
-            {this.state.error}
-          </Alert>
-          <SearchForm
-            handleFormSubmit={this.handleFormSubmit}
-            handleInputChange={this.handleInputChange}
-            breeds={this.state.breeds}
-          />
-          <SearchResults results={this.state.results} />
-        </Container>
-      </div>
+      // <div>
+      //   <Container style={{ minHeight: "80%" }}>
+      //     <h1 className="text-center">Search By Breed!</h1>
+      //     <Alert
+      //       type="danger"
+      //       style={{ opacity: this.state.error ? 1 : 0, marginBottom: 10 }}
+      //     >
+      //       {this.state.error}
+      //     </Alert>
+      //     <SearchForm
+      //       handleFormSubmit={this.handleFormSubmit}
+      //       handleInputChange={this.handleInputChange}
+      //       breeds={this.state.breeds}
+      //     />
+      //     <SearchResults results={this.state.results} />
+      //   </Container>
+      // </div>
     );
   }
 }
