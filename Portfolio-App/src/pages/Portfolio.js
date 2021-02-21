ed6075b9acec5fe89d30a33e44d95af3ada38b4a
Ian Wren <ian.wren81@gmail.com>
2021-02-21 13:17:00 -0800

    Formatted to take project component and cleaned look of code

--- Portfolio-App/src/pages/Portfolio.js
+++ Portfolio-App/src/pages/Portfolio.js
@@ -1,17 +1,13 @@
 import React, { Component } from "react";
 import API from "../utils/API";
 import Container from "../components/Container";
-import SearchForm from "../components/SearchForm";
-import SearchResults from "../components/SearchResults";
-import Alert from "../components/Alert";
+import Project from "../components/Project";
 
 class Portfolio extends Component {
-  // state = {
-  //   search: "",
-  //   breeds: [],
-  //   results: [],
-  //   error: ""
-  // };
+  state = {
+    projects: [],
+    error: ""
+  };
 
   // // When the component mounts, get a list of all available base breeds and update this.state.breeds
   // componentDidMount() {
@@ -37,6 +33,26 @@ class Portfolio extends Component {
   // };
   render() {
     return (
+      <div class="card" id="portfolioCard">
+        <div class="card-body" style="text-decoration-color: whitesmoke;">
+          <h1 style="text-decoration-color: whitesmoke;" class="card-title">Portfolio</h1>
+          <table class="table table-striped table-responsive align-middle">
+            <thead>
+              <tr>
+                <th scope="col" class="table-dark">Project</th>
+                <th scope="col" class="table-dark" id="tabletop">Example</th>
+                <th scope="col" class="table-dark">Description/Github Repo</th>
+              </tr>
+            </thead>
+            <tbody>
+              <Project/>
+            </tbody>
+
+          </table>
+
+        </div>
+      </div>
+
       // <div>
       //   <Container style={{ minHeight: "80%" }}>
       //     <h1 className="text-center">Search By Breed!</h1>
