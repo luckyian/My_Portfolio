f16ebe428f5d5c664f0c3282dde5295638142bb8
Ian Wren <ian.wren81@gmail.com>
2021-02-21 15:27:21 -0800

    Formatted project component to use props of data in projects.json called on the portfolio page.

--- Portfolio-App/src/components/Project/index.js
+++ Portfolio-App/src/components/Project/index.js
@@ -3,21 +3,19 @@ import { Link } from "react-router-dom";
 import "./style.css";
 
 
-function Project() {
+function Project(props) {
 
     return (
         <tr>
-            <th scope="row"><Link href="https://luckyian.github.io/whats-for-dinner/">What's for dinner? App</Link>
+            <th scope="row"><Link href={props.project}>{props.title}</Link>
             </th>
-            <td class="align-middle"><img src="./Assets/whatsfordinner.gif" alt="Group Project 1"
+            <td class="align-middle"><img src={props.src} alt={props.alt}
                 id="tabletop"></img>
 
             </td>
-            <td>The goal of this project was to create an application that will lift some of the burden of deciding what to make for dinner. The "What's for Dinner?" application 
-                    allows the user to search for online recipes based on ingredients he/she may have on-hand. Additionally, users will have the option to save recipes for future use, 
-                    which can easily be converted to a PDF to be used offline, or for the more tangible owners of the printer, printed out on paper.
+            <td>{props.text}
        
-                        <Link href="https://github.com/luckyian/whats-for-dinner">Github: https://github.com/luckyian/whats-for-dinner</Link>
+                        <Link href={props.github}>Github: {props.github}</Link>
             </td>
 
         </tr>
