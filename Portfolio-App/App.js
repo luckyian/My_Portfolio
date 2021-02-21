eb99ed5d916a6c9d07b9115395cc333f1e457c43
Ian Wren <ian.wren81@gmail.com>
2021-02-21 10:51:00 -0800

    Edited routes and import names to correct routes

--- Portfolio-App/App.js
+++ Portfolio-App/App.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Discover from "./pages/Discover";
-import About from "./pages/About";
-import Search from "./pages/Search";
+import Contact from "./pages/Contact";
+import Index from "./pages/Index";
+import Portfolio from "./pages/Portfolio";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
@@ -13,10 +13,10 @@ function App() {
       <div>
         <Navbar />
         <Wrapper>
-          <Route exact path="/" component={About} />
-          <Route exact path="/about" component={About} />
-          <Route exact path="/discover" component={Discover} />
-          <Route exact path="/search" component={Search} />
+          <Route exact path="/" component={Index} />
+          <Route exact path="/index" component={Index} />
+          <Route exact path="/contact" component={Contact} />
+          <Route exact path="/portfolio" component={Portfolio} />
         </Wrapper>
         <Footer />
       </div>
