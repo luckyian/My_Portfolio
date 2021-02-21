f011ba0c9c91fd41e13381674c465a5a5ef5e5b0
Ian Wren <ian.wren81@gmail.com>
2021-02-21 11:09:45 -0800

    Removed unneccessary boilerblate code

--- Portfolio-App/src/pages/Portfolio.js
+++ Portfolio-App/src/pages/Portfolio.js
@@ -5,36 +5,36 @@ import SearchForm from "../components/SearchForm";
 import SearchResults from "../components/SearchResults";
 import Alert from "../components/Alert";
 
-class Search extends Component {
-  state = {
-    search: "",
-    breeds: [],
-    results: [],
-    error: ""
-  };
+class Portfolio extends Component {
+  // state = {
+  //   search: "",
+  //   breeds: [],
+  //   results: [],
+  //   error: ""
+  // };
 
-  // When the component mounts, get a list of all available base breeds and update this.state.breeds
-  componentDidMount() {
-    API.getBaseBreedsList()
-      .then(res => this.setState({ breeds: res.data.message }))
-      .catch(err => console.log(err));
-  }
+  // // When the component mounts, get a list of all available base breeds and update this.state.breeds
+  // componentDidMount() {
+  //   API.getBaseBreedsList()
+  //     .then(res => this.setState({ breeds: res.data.message }))
+  //     .catch(err => console.log(err));
+  // }
 
-  handleInputChange = event => {
-    this.setState({ search: event.target.value });
-  };
+  // handleInputChange = event => {
+  //   this.setState({ search: event.target.value });
+  // };
 
-  handleFormSubmit = event => {
-    event.preventDefault();
-    API.getDogsOfBreed(this.state.search)
-      .then(res => {
-        if (res.data.status === "error") {
-          throw new Error(res.data.message);
-        }
-        this.setState({ results: res.data.message, error: "" });
-      })
-      .catch(err => this.setState({ error: err.message }));
-  };
+  // handleFormSubmit = event => {
+  //   event.preventDefault();
+  //   API.getDogsOfBreed(this.state.search)
+  //     .then(res => {
+  //       if (res.data.status === "error") {
+  //         throw new Error(res.data.message);
+  //       }
+  //       this.setState({ results: res.data.message, error: "" });
+  //     })
+  //     .catch(err => this.setState({ error: err.message }));
+  // };
   render() {
     return (
       <div>
