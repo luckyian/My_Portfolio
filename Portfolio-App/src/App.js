77d8600c8c55797d741ff2b6e873e0a74b252f30
Ian Wren <ian.wren81@gmail.com>
2021-02-21 21:52:08 -0800

    Renamed the footer import

--- Portfolio-App/src/App.js
+++ Portfolio-App/src/App.js
@@ -3,7 +3,7 @@ import { HashRouter as Router, Route } from "react-router-dom";
 // import Contact from "./pages/Contact";
 import Index from "./pages/Index";
 import Portfolio from "./pages/Portfolio";
-import Navbar from "./components/Navbar";
+import Navibar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <div>
-        <Navbar />
+        <Navibar />
         <Wrapper>
           <Route exact path="/" component={Index} />
           <Route exact path="/index" component={Index} />
