9e05fe1a722ec4e361b8ef69f35b70ff96aab382
Ian Wren <ian.wren81@gmail.com>
2021-02-21 20:08:11 -0800

    Refactored code for bootstrap react and commented out link to contact

--- Portfolio-App/src/components/Navbar/index.js
+++ Portfolio-App/src/components/Navbar/index.js
@@ -6,26 +6,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-md navbar-dark bg-dark">
+        <Navbar className="navbar navbar-expand-md navbar-dark bg-dark">
             <div className="col-md-9">
-                <a className="navbar-brand" href="#" style="background-color: rgb(48, 87, 55);">Ian Wren</a>
-                <button className="navbar-toggler  float-right" type="button" data-toggle="collapse"
+                <Navbar.Brand href="#" style="background-color: rgb(48, 87, 55);">Ian Wren</Navbar.Brand>
+                <Navbar.Toggle  className="float-right" type="button" data-toggle="collapse"
                     data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
+                    aria-label="Toggle navigation" />
             </div>
             <div className="col-md-3">
-                <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-                    <div className="navbar-nav">
-                        <Link to="/index" className={window.location.pathname === "/index" ? "nav-link active" : "nav-link"}>Home</Link>
-                        <Link to="/contact" className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>Contact</Link>
-                        <Link to="/portfolio" className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>Portfolio</Link>
-                        <Link to="#" className="nav-link" tabindex="3" aria-disabled="true"> </Link>
-                    </div>
-                </div>
+                <Navbar.Collapse id="navbarNavAltMarkup">
+                    <Nav>
+                        <Nav.Link to="/index" className={window.location.pathname === "/index" ? "nav-link active" : "nav-link"}>Home</Link>
+                        {/* <Nav.Link to="/contact" className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}>Contact</Link> */}
+                        <Nav.Link to="/portfolio" className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}>Portfolio</Link>
+                        <Nav.Link to="#" className="nav-link" tabindex="3" aria-disabled="true"> </Link>
+                    </Nav>
+                </Navbar.Collapse>
             </div>
-        </nav>
+        </Navbar>
     );
 }
 
