e13a1303650b56f4c9e0253a25321170f74102b9
Ian Wren <ian.wren81@gmail.com>
2021-02-21 21:56:18 -0800

    Changed react-bootstrap import call

--- Portfolio-App/src/components/Project/index.js
+++ Portfolio-App/src/components/Project/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
 
 
 function Project(props) {
