d45a3e6b06e4550e57442fb327964c42b3cf2858
Ian Wren <ian.wren81@gmail.com>
2021-02-21 16:09:45 -0800

    Removed instances of deleted folder structure

--- Portfolio-App/src/pages/Index.js
+++ Portfolio-App/src/pages/Index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Container from "../components/Container";
 import Row from "../components/Row";
-import Col from "../components/Col";
 
 function About() {
   return (
