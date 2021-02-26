15469e1fb766e47bc993a9320b3c2226690986f7
Ian Wren <ian.wren81@gmail.com>
2021-02-26 15:05:24 -0800

    Fixed route links for nav bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { HashRouter as Router, Route, Link } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Index from "./pages/Index";
 import Portfolio from "./pages/Portfolio";
