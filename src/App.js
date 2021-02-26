c8043c59a5bf62654fbf9653f9eaf45c78729081
Ian Wren <ian.wren81@gmail.com>
2021-02-26 12:39:56 -0800

    Fixed router requirements in app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { HashRouter as Router, Switch, Route, Link } from "react-router-dom";
-import { HashRouter as Router, Route, Link } from "react-router-dom";
 import Contact from "./pages/Contact";
 import Index from "./pages/Index";
 import Portfolio from "./pages/Portfolio";
