7d956bc4f2ee43846cce271c721b12787be41586
Ian Wren <ian.wren81@gmail.com>
2021-05-09 17:19:06 -0700

    Edited about me info and enlarged overall text

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -21,37 +21,36 @@ function Index() {
                 <Card.Img src="./assets/Ian.jpg" className="p-2" alt="Ian Wren"></Card.Img>
               </div>
               <div>
-             <p> My name is Ian Wren and I live in Seattle, WA.  I am 38 years old and
-                attend the University of Washington's full-stack coding bootcamp.
+             <p><h4> My name is Ian Wren and I live in Seattle, WA.  I am 38 years old and
+                am looking for work as a full-stack developer.
              <br></br>
              <br></br>
              I am looking to have a career as software developer with a focus on web
-             development. I currently work in retail where I have been in management
-             and trained, developed and coached associates to reach thier highest potential.
+             development. I currently work freelance for individuals and groups to design, build and deploy websites, and work with
+             a team of developers designing and building a mobile application using react native set to be released in September of 2021.
              <br></br>
              <br></br>
-             I am switching careers now to a vocation that challenges me on different
-             levels and allows me more time to spend with my loved ones, including my wife and
+             I would like to work full-time for a software firm that allows me more time to spend with my loved ones, including my wife and
              two feline fur-babies.
              <br></br>
              <br></br>
              Some of my hobbies include playing video games, gardening, home improvement on my current
              residence, hiking, trying new foods and drinks and travelling.  My youngest fur baby, Fox "Spooky" Mulder
              enjoys travelling with my wife, Drea, and I and he also enjoys hikes while on a leash.
-             </p>
+             </h4></p>
              <br></br>
              <br></br>
              <p>
-                  <h3><u>Contact Info</u></h3>
-                  <h4>Email: ian.wren81@gmail.com</h4>
-                  <h4>Phone: Contained in Resume</h4>
-                  <h4><a href="https://drive.google.com/file/d/1repu1_TLSkM5UwfJFO3LysVA6wazq6Is/view?usp=sharing">Resume</a></h4>
-                  <h4>
+                  <h2><u>Contact Info</u></h2>
+                  <h3>Email: ian.wren81@gmail.com</h3>
+                  <h3>Phone: Contained in Resume</h3>
+                  <h3><a href="https://drive.google.com/file/d/1repu1_TLSkM5UwfJFO3LysVA6wazq6Is/view?usp=sharing">Resume</a></h3>
+                  <h3>
                     <a href="https://linkedin.com/in/ian-wren-53b382112">LinkedIn</a>
-                  </h4>
-                  <h4>
+                  </h3>
+                  <h3>
                     <a href="https://github.com/luckyian">GitHub</a>
-                  </h4>
+                  </h3>
 
               </p>
 
