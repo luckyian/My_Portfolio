e2eaf4acfdc1920f8b6429fc813c8372502604f5
luckyian <ian.wren81@gmail.com>
2023-04-06 15:10:25 -0700

    Updated link to resume

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -45,7 +45,7 @@ function Index() {
                   <h2><u>Contact Info</u></h2>
                   <h3>Email: ian.wren81@gmail.com</h3>
                   <h3>Phone: Contained in Resume</h3>
-                  <h3><a href="https://1drv.ms/b/s!AnD0ThTIlXlzg4RGGylzkCmabdEbGw?e=RjNSBd">Resume</a></h3>
+                  <h3><a href="https://1drv.ms/b/s!AnD0ThTIlXlzg4RGGylzkCmabdEbGw?e=mAC0BG">Resume</a></h3>
                   <h3>        
                     <a href="https://www.linkedin.com/in/ian-wren/">LinkedIn</a>
                   </h3>
