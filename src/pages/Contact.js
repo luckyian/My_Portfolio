55b047ccf6120dc759db14cf605e66df9f15e4cc
luckyian <ian.wren81@gmail.com>
2024-10-26 21:43:17 -0700

    Imported data from analytics data folder

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,71 +4,16 @@ import Card from "react-bootstrap/Card";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import { Container } from "react-bootstrap";
+import Project from "../components/Project";
+import data from "../data/analytics.json";
 
 function Contact() {
 
   return (
     <Container>
-    <div>
-
-      <Card className="aboutBack">
-<Card.Header>
-
-</Card.Header>
-<Card.Body>
-  <Card.Title>
-    
-  </Card.Title>
-</Card.Body>
-      </Card>
-      {/* <br></br>
-    <Card className="aboutBack">
-      <Card.Body>
-        <Card.Title>
-          <h1>Contact</h1>
-
-        </Card.Title>
-
-        <Form>
-          <Form.Group>
-            <Form.Label for="exampleFormControlInput1">Name</Form.Label>
-            <Form.Control type="email" className="form-control" id="exampleFormControlInput1"
-              placeholder="Jane Doe"></Form.Control>
-          </Form.Group>
-          <Form.Group>
-            <Form.Label for="exampleFormControlInput1">Email address</Form.Label>
-            <Form.Control type="email" className="form-control" id="exampleFormControlInput1"
-              placeholder="name@example.com"></Form.Control>
-          </Form.Group>
-
-          <Form.Group>
-            <Form.Label for="exampleFormControlTextarea1">Questions:</Form.Label>
-            <Form.Control id="exampleFormControlTextarea1" rows="3"></Form.Control>
-          </Form.Group>
-          <Button type="submit" className="btn btn-primary">Submit</Button>
-        </Form>
-
-      </Card.Body>
-      <br></br>
-
-
+    <Card>
 
     </Card>
-    <br></br> */}
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-<br></br>
-    
-    </div>
     </Container>
 
   );
